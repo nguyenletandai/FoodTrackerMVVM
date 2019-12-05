f3b9e3df633627144f4d76585ec2b42ec166f750
daiNLT <dainlt@rikkeisoft.com>
2019-12-05 14:01:38 +0700

    Initial Commit

---
+++ FoodTrackerMVVM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerMVVM
+//
+//  Created by DaiNLT-D3-M on 12/5/19.
+//  Copyright © 2019 DaiNLT-D3-M. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
